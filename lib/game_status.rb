26d5b510179d73e1e2f5a7261e4e2cc7784515d0
Andrew Eames <ajerxs@gmail.com>
2020-10-27 02:26:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,7 +29,7 @@ def won?(board)
     position_3 = board[win_index_3]
     position_4 = board[win_index_4]
 
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    if position_1 == "X" && position_2 == "X" && position_3 == "X" && position_4 == "X"
       return win_combination
     end
   end
