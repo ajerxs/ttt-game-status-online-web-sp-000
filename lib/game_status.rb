b0c9bd68aabeb3a1eed597330255125e19452822
Andrew Eames <ajerxs@gmail.com>
2020-10-27 03:46:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -51,12 +51,8 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board) == "X"
-    return "X"
-  elsif won?(board) == "O"
-    return "O"
-  else
-    return false
+  if won?(board)
+    return board[won?(board)[0]]
   end
 end
     
